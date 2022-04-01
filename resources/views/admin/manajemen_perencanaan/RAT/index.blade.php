48de054bcca48a1602b69e5a89b209b8d7b37808
madajabbar <madajabbar22@gmail.com>
2022-04-01 17:12:26 +0700

    fix

--- resources/views/admin/manajemen_perencanaan/RAT/index.blade.php
+++ resources/views/admin/manajemen_perencanaan/RAT/index.blade.php
@@ -1,18 +1,20 @@
 @extends('layouts.admin')
 @section('css')
-<link rel="stylesheet" href="{{asset('assets/vendors/jquery-datatables/jquery.dataTables.bootstrap5.min.css')}}">
-<link rel="stylesheet" href="{{asset('assets/vendors/fontawesome/all.min.css')}}">
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>
+    <link rel="stylesheet" href="{{ asset('assets/vendors/jquery-datatables/jquery.dataTables.bootstrap5.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/vendors/fontawesome/all.min.css') }}">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>
 
-<meta name="csrf-token" content="{{ csrf_token() }}">
-<style>
-    table.dataTable td{
-        padding: 15px 8px;
-    }
-    .fontawesome-icons .the-icon svg {
-        font-size: 24px;
-    }
-</style>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <style>
+        table.dataTable td {
+            padding: 15px 8px;
+        }
+
+        .fontawesome-icons .the-icon svg {
+            font-size: 24px;
+        }
+
+    </style>
 @endsection
 @section('content')
     <div id="main">
@@ -23,14 +25,14 @@
         </header>
 
         <div class="page-heading">
-            <h3>{{$title}}</h3>
+            <h3>{{ $title }}</h3>
             <!-- Button trigger for login form modal -->
 
         </div>
         <div class="page-content">
             <div class="card">
                 <div class="card-header">
-                    {{$subtitle}}
+                    {{ $subtitle }}
                     <br>
                     <button type="button" class="btn rounded-pill btn-outline-warning" data-bs-toggle="modal"
                         data-bs-target="#inlineForm">
@@ -89,145 +91,146 @@
 @endsection
 
 @section('js')
-<script src="{{asset('assets/vendors/jquery/jquery.min.js')}}"></script>
-<script src="{{asset('assets/vendors/jquery-datatables/jquery.dataTables.min.js')}}"></script>
-<script src="{{asset('assets/vendors/jquery-datatables/custom.jquery.dataTables.bootstrap5.min.js')}}"></script>
-<script src="{{asset('assets/vendors/fontawesome/all.min.js')}}"></script>
+    <script src="{{ asset('assets/vendors/jquery/jquery.min.js') }}"></script>
+    <script src="{{ asset('assets/vendors/jquery-datatables/jquery.dataTables.min.js') }}"></script>
+    <script src="{{ asset('assets/vendors/jquery-datatables/custom.jquery.dataTables.bootstrap5.min.js') }}"></script>
+    <script src="{{ asset('assets/vendors/fontawesome/all.min.js') }}"></script>
 
 
-<script type="text/javascript">
-    // Jquery Datatable
-    $(function (){
-        $.ajaxSetup({
-          headers: {
-              'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-          }
-    });
+    <script type="text/javascript">
+        // Jquery Datatable
+        $(function() {
+            $.ajaxSetup({
+                headers: {
+                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                }
+            });
 
-        var jquery_datatable = $("#table1").DataTable({
-            processing: true,
-            serverSide: true,
-            ajax: "{{route('assign-assessment.index')}}",
-            columns:
-                [{
-                data: 'DT_RowIndex',
-                orderable: false
-                },
-                {
-                    data: 'periode',
-                    orderable: true
-                },
-                {
-                    data: 'auditee',
-                    orderable: true
-                },
-                {
-                    data: 'jenis_assessment',
-                    orderable: true
-                },
+            var jquery_datatable = $("#table1").DataTable({
+                processing: true,
+                serverSide: true,
+                ajax: "{{ route('assign-assessment.index') }}",
+                columns: [{
+                        data: 'DT_RowIndex',
+                        orderable: false
+                    },
+                    {
+                        data: 'periode',
+                        orderable: true
+                    },
+                    {
+                        data: 'auditee',
+                        orderable: true
+                    },
+                    {
+                        data: 'jenis_assessment',
+                        orderable: true
+                    },
 
-                {
-                    data: 'pka',
-                    orderable: true
-                },
-                {
-                    data: 'auditor',
-                    orderable: true
-                },
-                {
-                    data: 'action',
-                    name: '#',
-                    orderable: false
-                },
-            ],
-            order: [[0, 'desc']]
-        })
+                    {
+                        data: 'pka',
+                        orderable: true
+                    },
+                    {
+                        data: 'auditor',
+                        orderable: true
+                    },
+                    {
+                        data: 'action',
+                        name: '#',
+                        orderable: false
+                    },
+                ],
+                order: [
+                    [0, 'desc']
+                ]
+            })
 
-    });
-    $('body').on('click', '.deleteProduct', function () {
+        });
+        $('body').on('click', '.deleteProduct', function() {
 
-        var data_id = $(this).data("id");
-        confirm("Are You sure want to delete !");
+            var data_id = $(this).data("id");
+            confirm("Are You sure want to delete !");
 
-        $.ajax({
-            type: "DELETE",
-            url: "{{ route('assign-assessment.store') }}"+'/'+data_id,
-            success: function (data) {
-                reloadDatatable();
-                Swal.fire({
+            $.ajax({
+                type: "DELETE",
+                url: "{{ route('assign-assessment.store') }}" + '/' + data_id,
+                success: function(data) {
+                    reloadDatatable();
+                    Swal.fire({
                         icon: 'warning',
                         title: 'Data berhasil dihapus',
-                        })
-            },
-            error: function (data) {
-                console.log('Error:', data);
-            }
+                    })
+                },
+                error: function(data) {
+                    console.log('Error:', data);
+                }
+            });
         });
-    });
-    $('body').on('click', '.editProduct', function () {
-      var data_id = $(this).data('id');
-      $.get("{{ route('assign-assessment.index') }}" +'/' + data_id +'/edit', function (data) {
-          $('#myModalLabel33').html("Edit Risk Assessment");
-          $('#saveBtn').val("edit");
-          $('#inlineForm').modal('show');
-          $('#id').val(data.id);
-          $('#periode').val(data.periode);
-          $('#jenis_assessment').val(data.jenis_assessment);
-          $('#auditee').val(data.auditee);
-          $('#auditor').val(data.auditor);
-          $('#pka').val(data.pka);
-      })
-   });
+        $('body').on('click', '.editProduct', function() {
+            var data_id = $(this).data('id');
+            $.get("{{ route('assign-assessment.index') }}" + '/' + data_id + '/edit', function(data) {
+                $('#myModalLabel33').html("Edit Risk Assessment");
+                $('#saveBtn').val("edit");
+                $('#inlineForm').modal('show');
+                $('#id').val(data.id);
+                $('#periode').val(data.periode);
+                $('#jenis_assessment').val(data.jenis_assessment);
+                $('#auditee').val(data.auditee);
+                $('#auditor').val(data.auditor);
+                $('#pka').val(data.pka);
+            })
+        });
 
-    function reloadDatatable() {
-        $('#table1').DataTable().ajax.reload();
-    }
-</script>
-<script type="text/javascript">
-    $(function(){
-        $.ajaxSetup({
-            headers: {
-                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-            }
-        });
-        $('#saveBtn').click(function (e) {
+        function reloadDatatable() {
+            $('#table1').DataTable().ajax.reload();
+        }
+    </script>
+    <script type="text/javascript">
+        $(function() {
+            $.ajaxSetup({
+                headers: {
+                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                }
+            });
+            $('#saveBtn').click(function(e) {
                 e.preventDefault();
                 $(this).html('Sending..');
                 var myform = document.getElementById('dataForm');
                 var formData = new FormData(myform);
 
                 $.ajax({
-                data: formData,
-                cache:false,
-                contentType: false,
-                processData: false,
-                url: "{{ route('assign-assessment.store') }}",
-                type: "POST",
-                dataType: 'json',
+                    data: formData,
+                    cache: false,
+                    contentType: false,
+                    processData: false,
+                    url: "{{ route('assign-assessment.store') }}",
+                    type: "POST",
+                    dataType: 'json',
 
-                success: function (data) {
+                    success: function(data) {
 
-                    reloadDatatable();
-                    $('#dataForm').trigger("reset");
-                    $('#ajaxModel').modal('hide');
-                    $('#saveBtn').html('success');
+                        reloadDatatable();
+                        $('#dataForm').trigger("reset");
+                        $('#ajaxModel').modal('hide');
+                        $('#saveBtn').html('success');
 
-                    Swal.fire({
-                        icon: 'success',
-                        title: 'Data berhasil dimasukan',
+                        Swal.fire({
+                            icon: 'success',
+                            title: 'Data berhasil dimasukan',
                         })
-                },
-                error: function (data) {
-                    console.log('Error:', data);
-                    Swal.fire({
-                        icon: 'error',
-                        title: 'Terjadi Error!',
+                    },
+                    error: function(data) {
+                        console.log('Error:', data);
+                        Swal.fire({
+                            icon: 'error',
+                            title: 'Terjadi Error!',
                         })
-                    $('#saveBtn').html('Error');
-                }
+                        $('#saveBtn').html('Error');
+                    }
+                });
             });
-        });
 
-    });
-</script>
+        });
+    </script>
 @endsection
